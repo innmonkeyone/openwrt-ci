8d2730e8a9806997d0fb4c3438ef89e72ceff465
innmonkeyone <houjialiangchn@163.com>
2025-11-28 20:50:27 +0800

    更新 diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -15,7 +15,7 @@ function git_sparse_clone() {
 # 添加额外插件
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 #git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 #git_sparse_clone master https://github.com/kenzok8/openwrt-packages luci-app-adguardhome adguardhome luci-app-wechatpush
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-onliner luci-app-floatip floatip luci-app-adguardhome adguardhome
@@ -26,10 +26,10 @@ git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 echo "
 # 插件
 #CONFIG_PACKAGE_luci-app-smartdns=y
-CONFIG_PACKAGE_luci-app-openclash=m
+#CONFIG_PACKAGE_luci-app-openclash=m
 CONFIG_PACKAGE_luci-app-adguardhome=y
 CONFIG_PACKAGE_luci-app-wechatpush=y
-CONFIG_PACKAGE_luci-app-lucky=y
+#CONFIG_PACKAGE_luci-app-lucky=y
 CONFIG_PACKAGE_luci-app-onliner=y
 CONFIG_PACKAGE_luci-app-floatip=y
 " >> .config
