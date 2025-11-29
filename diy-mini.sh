e28cef51cdd6d780668bd48ac2532b5262f313b6
innmonkeyone <houjialiangchn@163.com>
2025-11-29 21:47:17 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -16,21 +16,23 @@ function git_sparse_clone() {
 #git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 #git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
-#git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-#git_sparse_clone master https://github.com/kenzok8/openwrt-packages luci-app-adguardhome adguardhome luci-app-wechatpush
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-onliner luci-app-floatip floatip
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-adguardhome adguardhome
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone master https://github.com/kenzok8/openwrt-packages luci-app-adguardhome adguardhome
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-floatip floatip
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-onliner luci-app-floatip floatip luci-app-adguardhome adguardhome
+#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-floatip floatip
+
+
 # MosDNS
 #git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 echo "
 # 插件
-#CONFIG_PACKAGE_luci-app-smartdns=y
-#CONFIG_PACKAGE_luci-app-openclash=m
-#CONFIG_PACKAGE_luci-app-adguardhome=y
+CONFIG_PACKAGE_luci-app-openclash=m
+CONFIG_PACKAGE_luci-app-adguardhome=y
 CONFIG_PACKAGE_luci-app-wechatpush=y
-#CONFIG_PACKAGE_luci-app-lucky=y
-#CONFIG_PACKAGE_luci-app-onliner=y
+CONFIG_PACKAGE_luci-app-lucky=y
+CONFIG_PACKAGE_luci-app-onliner=y
 #CONFIG_PACKAGE_luci-app-floatip=y
 " >> .config
 
