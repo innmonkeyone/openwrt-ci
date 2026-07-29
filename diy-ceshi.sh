d2dc48a50c8c82af167d3af4c5953a502283a84f
innmonkeyone <houjialiangchn@163.com>
2026-07-29 13:09:25 +0800

    Update diy-ceshi.sh

--- diy-ceshi.sh
+++ diy-ceshi.sh
@@ -15,30 +15,15 @@ function git_sparse_clone() {
 }
 
 # 添加额外插件
-#git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-#git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
-#git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
-#git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-git_sparse_clone master https://github.com/kenzok8/openwrt-packages luci-app-adguardhome adguardhome
-git_sparse_clone main https://github.com/kiddin9/op-packages  luci-app-lucky lucky
-
-# MosDNS
-#git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-
-# 加入新储存库
-echo "src-git rtp2httpd https://github.com/stackia/rtp2httpd.git" >> "feeds.conf.default"
-
-
+git clone https://github.com/sbwml/luci-app-quickfile package/quickfile
 
 echo "
 # 插件
 CONFIG_PACKAGE_luci-app-adguardhome=y
-CONFIG_PACKAGE_luci-app-lucky=y
 CONFIG_PACKAGE_luci-app-rtp2httpd=y
 CONFIG_PACKAGE_luci-app-quickfile=y
 CONFIG_PACKAGE_luci-app-wechatpush=y
 CONFIG_PACKAGE_luci-app-autoreboot=y
-CONFIG_NET_PHY_AQUANTIA=y
 " >> .config
 
 # 修改默认IP
