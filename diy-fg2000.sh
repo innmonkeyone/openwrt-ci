62e8c8954c1c9b2fd5bc6d22251c8028f64fcace
innmonkeyone <houjialiangchn@163.com>
2026-08-04 17:47:58 +0800

    Update diy-fg2000.sh

--- diy-fg2000.sh
+++ diy-fg2000.sh
@@ -35,15 +35,14 @@ echo "src-git rtp2httpd https://github.com/stackia/rtp2httpd.git" >> "feeds.conf
 echo "
 # 插件
 CONFIG_PACKAGE_luci-app-adguardhome=y
-#CONFIG_PACKAGE_luci-app-floatip=y
-CONFIG_PACKAGE_luci-app-lucky=y
 CONFIG_PACKAGE_luci-app-onliner=y
+CONFIG_PACKAGE_luci-app-wechatpush=y
 CONFIG_PACKAGE_luci-app-rtp2httpd=y
 CONFIG_NET_PHY_AQUANTIA=y
 " >> .config
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.252/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
